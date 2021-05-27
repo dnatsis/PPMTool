00791add3cf290f6b5349f28de511d761afc7d75
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-05-27 00:17:50 -0400

    Added Component structure and created boilerplate components

---
+++ ppmtool-client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProjectItem from './Project/ProjectItem';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <ProjectItem />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ ppmtool-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectItem = () => {
+  return (
+    <div>
+      <h1>Project Item</h1>
+    </div>
+  );
+};
+
+export default ProjectItem;
