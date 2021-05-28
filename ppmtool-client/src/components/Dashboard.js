e0ab1356e4fecdb2becae209bca14a6f96f5c751
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-05-28 11:56:01 -0400

    Created Dashboard and ProjectItem Component

--- ppmtool-client/src/components/Dashboard.js
+++ ppmtool-client/src/components/Dashboard.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ProjectItem from './Project/ProjectItem';
+import { Container, Row, Col, Card, Button, ListGroup } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Welcome to the Dashboard</h1>
-      <ProjectItem />
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <h1 className="display-4 text-center">Projects</h1>
+            <br />
+            <Button className="btn-lg" variant="info">
+              Create a Project
+            </Button>
+            <br />
+            <hr />
+
+            <ProjectItem />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
--- ppmtool-client/src/components/Project/ProjectItem.js
+++ ppmtool-client/src/components/Project/ProjectItem.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Container, Row, Col, Card, Button, ListGroup } from 'react-bootstrap';
 
 const ProjectItem = () => {
   return (
-    <div>
-      <h1>Project Item</h1>
-    </div>
+    <>
+      <Container>
+        <Card className="bg-light mb-3">
+          <Card.Body>
+            <Row>
+              <Col md={2}>
+                <span className="mx-auto">REACT</span>
+              </Col>
+              <Col lg={6} md={4}>
+                <Card.Title>Spring / React Project</Card.Title>
+                <Card.Text>
+                  Project to create a kanban board with spring boot and react
+                </Card.Text>
+              </Col>
+              <Col md={4} className="d-none d-lg-block">
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <i
+                      className="fas fa-project-diagram"
+                      style={{ paddingRight: '10px' }}
+                    ></i>
+                    <Button className="btn-md" variant="primary">
+                      Project Board
+                    </Button>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <i
+                      className="fa fa-edit pr-1"
+                      style={{ paddingRight: '10px' }}
+                    ></i>
+                    <Button className="btn-md" variant="success">
+                      Update Project Info
+                    </Button>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <i
+                      class="fa fa-minus-circle pr-1"
+                      style={{ paddingRight: '10px' }}
+                    ></i>
+                    <Button className="btn-md" variant="danger">
+                      Delete Project
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+    </>
   );
 };
 
