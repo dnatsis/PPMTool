97b8ecf715387ce93d704c396a153b37ce399789
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-06-03 12:45:56 -0400

    BacklogController Get Mapping getProjectBacklog with id

--- PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/ProjectTaskService.java
+++ PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/ProjectTaskService.java
@@ -42,4 +42,8 @@ public class ProjectTaskService {
         return projectTaskRepository.save(projectTask);
 
     }
+
+    public Iterable<ProjectTask>findBacklogById(String backlog_id) {
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+    }
 }
