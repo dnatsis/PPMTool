3f0e405778ed92c9bba35252d23ec670e76db134
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-06-02 20:32:11 -0400

    Backlog and ProjectTask Objects and Repositories

---
+++ PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,11 @@
+package com.dhimitrisprojects.ppmtool.repositories;
+
+import com.dhimitrisprojects.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+}
--- PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.dhimitrisprojects.ppmtool.services;
 
+import com.dhimitrisprojects.ppmtool.domain.Backlog;
 import com.dhimitrisprojects.ppmtool.domain.Project;
 import com.dhimitrisprojects.ppmtool.exceptions.ProjectIdException;
+import com.dhimitrisprojects.ppmtool.repositories.BacklogRepository;
 import com.dhimitrisprojects.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,25 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project) {
 
         try {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if (project.getId() == null) {
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if (project.getId() != null) {
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
             return projectRepository.save(project);
 
         } catch (Exception e){
