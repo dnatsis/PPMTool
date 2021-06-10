71841a65b144badd3d8934f3448f39fb6a477d66
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-06-09 23:49:53 -0400

    UsernameAlreadyExists Exception, and Response

--- PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/UserService.java
+++ PPMTool/src/main/java/com/dhimitrisprojects/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.dhimitrisprojects.ppmtool.services;
 
 import com.dhimitrisprojects.ppmtool.domain.User;
+import com.dhimitrisprojects.ppmtool.exceptions.UsernameAlreadyExistsException;
 import com.dhimitrisprojects.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,10 +17,17 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
-        //username is unique(exception)
-        //make sure that password and confirmPassword match
-        //we dont persist or show the confirmPassword
-        return userRepository.save(newUser);
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //username is unique(exception)
+            newUser.setUsername(newUser.getUsername());
+            //make sure that password and confirmPassword match
+            //we dont persist or show the confirmPassword
+            return userRepository.save(newUser);
+
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists!");
+        }
+
     }
 }
